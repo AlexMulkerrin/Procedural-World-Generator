55421c041a1d11f9228d9cb589daf3ba68bf0999
Alex Mulkerrin <ajmulkerrin@gmail.com>
2022-07-04 06:51:36 +0100

    added default faction colours
    selection of default colours for first 16 factions that contrast well with each other

--- JavaScript/Simulation/Faction.js
+++ JavaScript/Simulation/Faction.js
@@ -1,8 +1,14 @@
+const defaultFactionColours = ["#798EFF", "#4CFF00", "#FFD800", "#FF0000", "#B200FF", "#FF6A00", "#FFFFFF", "#606060", "#22B14C", "#880015", "#99D9EA", "#FFC1FF", "#3F48CC", "#7F6A00", "#FF00FF", "#FFF5AF"];
+
+const factionColourNames = ["blue", "lime", "yellow", "red", "purple", "orange", "white", "black", "green", "dark red", "light blue", "light pink", "dark blue", "brown", "magenta", "light yellow"];
 
 function Faction(inID) {
 	this.id = inID;
 	this.name = randomName();
 	this.colour = randomColour();
+	if (this.id < defaultFactionColours.length) {
+		this.colour = defaultFactionColours[this.id];
+	}
 
 	this.totalStructures = 0;
 	this.totalPop = 0;
